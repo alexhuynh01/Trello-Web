80582a7668946be4f209bc170120aa197f590507
alexhuynh <121414952+alexhuynh01@users.noreply.github.com>
2024-03-30 15:22:21 +0700

    add Layout

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,16 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
-import { cyan, deepOrange, green, orange } from '@mui/material/colors'
+import { cyan, deepOrange, teal, orange } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
+  trello: {
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
+  },
   colorSchemes: {
     light: {
       palette: {
-        primary: green,
+        primary: teal,
         secondary: deepOrange
       }
     },
